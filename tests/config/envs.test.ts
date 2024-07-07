8d9eac7f6d69f0d909cd74d79093c0c87bcec2bc
JKmoises <moi.prado@duocuc.cl>
2024-07-06 21:00:36 -0400

    Probar variables de entorno de envs.ts

---
+++ tests/config/envs.test.ts
@@ -0,0 +1,19 @@
+import { envs } from '../../netlify/config/envs';
+
+describe("Probar envs.ts", () => {
+  test('Debe retornar las variables de .env.test', () => {
+    const mockEnv = {
+      DATABASE_URL: "postgresql://mcreaciones_owner:vKi8mSx1AheN@ep-twilight-base-a5xovb1p-pooler.us-east-2.aws.neon.tech/mcreaciones?sslmode=require",
+      JWT_SEED: "3d7c8a7dd285fe8d3c8f55f8fa5b3f9b4c5a1864f2f8e6c8a2c9f4f8fa8b9f4c2a5b8f9b4c5a1864f2f8e6c8a2c9f4f8fa8b9f4c",
+      SEND_EMAIL: true,
+      MAILER_HOST: "smtp.gmail.com",
+      MAILER_EMAIL: "prueba@gmail.com",
+      MAILER_PORT: 465,
+      MAILER_USER: "prueba@gmail.com",
+      MAILER_SECRET_KEY: "secret_key_prueba",
+      FRONTEND_URL: "http://localhost:5173",
+    };
+
+    expect(envs).toEqual(mockEnv);
+  });
+ });
--- tests/functions/hello/hello.test.ts
+++
@@ -1,8 +0,0 @@
-import { envs } from "../../../netlify/config/envs";
-
-describe("hello function", () => {
-  test("returns a greeting", async () => {
-    console.log("Probando test de hello.ts");
-   console.log(envs.DATABASE_URL);
-  });
-});
